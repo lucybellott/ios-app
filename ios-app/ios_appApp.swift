12d4c8900b6b1f67445d55ab4cbfcf56bff91dd4
Lucy Bellott <lbellott@tripadvisor.com>
2024-06-18 13:42:07 -0400

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Lucy Bellott on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
