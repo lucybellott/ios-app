0121a00e9cf3894db44717f8f62974628e9d3b38
Lucy Bellott <lbellott@tripadvisor.com>
2024-06-19 18:11:57 -0400

    weather data added

---
+++ ios-app/Views/WeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherView.swift
+//  ios-app
+//
+//  Created by Lucy Bellott on 6/19/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
